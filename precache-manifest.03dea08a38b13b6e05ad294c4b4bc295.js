e250303cd580ba2fb5fe7a67708884425fa7b57c
Bilal <Bilal.aslam.sheikh@gmail.com>
2020-06-15 21:03:51 -0400

    Updates

---
+++ precache-manifest.03dea08a38b13b6e05ad294c4b4bc295.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5571803a1624f4755c6f42f5e7e056f",
+    "url": "/Robofriends/index.html"
+  },
+  {
+    "revision": "3bcb95ff542e94047ead",
+    "url": "/Robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cc5d5fcdee270fb22c82",
+    "url": "/Robofriends/static/css/main.b551e5a0.chunk.css"
+  },
+  {
+    "revision": "3bcb95ff542e94047ead",
+    "url": "/Robofriends/static/js/2.3ab5c44c.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/Robofriends/static/js/2.3ab5c44c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc5d5fcdee270fb22c82",
+    "url": "/Robofriends/static/js/main.b6f921a9.chunk.js"
+  },
+  {
+    "revision": "4eac723178dd1eab715f",
+    "url": "/Robofriends/static/js/runtime-main.33ca4b66.js"
+  }
+]);
